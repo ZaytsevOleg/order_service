702ac617278b521b777282139e1b4c7faa3697b7
Олег Зайцев <zo@guinot-spa.ru>
2026-08-12 14:55:28 +0300

    Add brand themes

---
+++ order_service/catalog/context_processors.py
@@ -0,0 +1,30 @@
+from django.templatetags.static import static
+
+
+def brand_context(request):
+    site = getattr(request, "brand_site", None) or {}
+
+    code = site.get("code", "default")
+
+    if code == "guinot":
+        return {
+            "brand_code": "guinot",
+            "brand_title": "Guinot",
+            "brand_theme_css": "orders/themes/guinot.css",
+            "brand_logo": "orders/img/guinot/logo.png",
+        }
+
+    if code == "rhea":
+        return {
+            "brand_code": "rhea",
+            "brand_title": "Rhea Cosmetics",
+            "brand_theme_css": "orders/themes/rhea.css",
+            "brand_logo": "orders/img/rhea/logo.png",
+        }
+
+    return {
+        "brand_code": "default",
+        "brand_title": "Order Service",
+        "brand_theme_css": "orders/themes/default.css",
+        "brand_logo": "",
+    }
