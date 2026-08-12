30d5f4014e1ca6bbf684f6f259e4d73cca554df5
Олег Зайцев <zo@guinot-spa.ru>
2026-08-12 17:27:15 +0300

    edit order_create

--- order_service/catalog/context_processors.py
+++ order_service/catalog/context_processors.py
@@ -17,31 +17,24 @@ def brand_context(request):
         return {
             "brand_code": "guinot",
             "brand_title": "Guinot",
-            "brand_theme_css": (
-                "orders/themes/guinot.css"
-            ),
-            "brand_logo": (
-                "orders/img/guinot/logo.png"
-            ),
+            "brand_theme_css": "orders/themes/guinot.css",
+            "brand_logo": "orders/img/guinot/logo.png",
+            "brand_favicon": "orders/img/guinot/favicon.png",
         }
 
     if code == "rhea":
         return {
             "brand_code": "rhea",
             "brand_title": "Rhea Cosmetics",
-            "brand_theme_css": (
-                "orders/themes/rhea.css"
-            ),
-            "brand_logo": (
-                "orders/img/rhea/logo.png"
-            ),
+            "brand_theme_css": "orders/themes/rhea.css",
+            "brand_logo": "orders/img/rhea/logo.png",
+            "brand_favicon": "orders/img/rhea/favicon.png",
         }
 
     return {
         "brand_code": "default",
         "brand_title": "Order Service",
-        "brand_theme_css": (
-            "orders/themes/default.css"
-        ),
+        "brand_theme_css": "orders/themes/default.css",
         "brand_logo": "",
+        "brand_favicon": "",
     }
